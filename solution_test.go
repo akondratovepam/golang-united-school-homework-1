770bf3c77bb4bc83e167dc438042bd15cbbedc1c
Andrey Kondratov <andrey.kondratov@ihsmarkit.com>
2022-04-29 12:51:25 +0300

    Removed test contents.

--- solution_test.go
+++ solution_test.go
@@ -2,15 +2,7 @@ package solution
 
 import (
 	"testing"
-
-	"github.com/kyokomi/emoji"
 )
 
 func TestGetMessage(t *testing.T) {
-	expected := emoji.Sprint("Hello, :world_map:!")
-	actual := GetMessage()
-
-	if actual != expected {
-		t.Fatalf(`GetMessage() = %q, want %q`, actual, expected)
-	}
 }
